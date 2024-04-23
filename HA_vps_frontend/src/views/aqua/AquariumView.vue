fd577e7d3f5da5f9c4f9d30adbfc90fe38eb42c3
Diptanil Chaudhuri <diptanil.chaudhuri@gmail.com>
2024-04-23 11:56:34 -0500

    Added cards to garden and aquarium pages

--- HA_vps_frontend/src/views/aqua/AquariumView.vue
+++ HA_vps_frontend/src/views/aqua/AquariumView.vue
@@ -5,11 +5,43 @@
       <div v-else>
         <div v-if="error" class="error">Error: {{ error }}</div>
         <div v-else>
-            {{ tanks }}
+            <div class="flex flex-wrap justify-around">
+
+              <div class="w-1/4 px-3 py-3" v-for="tank in tanks" :key="tank.id">
+                
+                <RouterLink :to="{name: 'plant', params: {id: tank.uuid}}">
+                  <fwb-card href="#">
+                        
+                    <div class="p-5 w-35">        
+                        <img class="scale-50" src="../../assets/icons/fish_11792940.png" alt="">     
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        {{ tank.name }}
+                        </h5>
+                        <!-- <fwb-badge> {{ plant.location }} </fwb-badge> -->
+                        <p class="font-normal text-gray-700 dark:text-gray-400">
+                        {{ tank.description }}
+                        </p>
+                    </div>
+
+                  </fwb-card>
+                </RouterLink>
+
+              </div>
+              </div>
+
         </div>
       </div>
     </div>
   </template>
+
+<script setup>
+import { 
+   FwbCard,
+   FwbBadge,
+   FwbSpinner,
+   FwbImg,
+  } from 'flowbite-vue'
+</script>
   
   <script>
   import axios from 'axios';
--- HA_vps_frontend/src/views/garden/GardenView.vue
+++ HA_vps_frontend/src/views/garden/GardenView.vue
@@ -1,29 +1,56 @@
 <template>
-    <div class="py-10 px-8 border-b border-gray-200">
-      <h1>Garden View</h1>
-      <div v-if="loading">Loading...</div>
+  <div class="py-10 px-8 border-b border-gray-200">
+
+    <div v-if="loading"><fwb-spinner /></div>
+    <div v-else>
+      <div v-if="error" class="error">Error: {{ error }}</div>
       <div v-else>
-        <div v-if="error" class="error">Error: {{ error }}</div>
-        <div v-else>
-            {{ plants }}
-          <!-- <div v-for="plant in plants" :key="plant.id"> -->
-            <!-- <h2>{{ plant }}</h2> -->
-            <!-- <p>{{ plant.description }}</p> -->
-          <!-- </div> -->
+        <div class="flex flex-wrap justify-around">
+
+          <div class="w-1/4 px-3 py-3" v-for="plant in plants" :key="plant.id">
+            
+            <RouterLink :to="{name: 'plant', params: {id: plant.uuid}}">
+              <fwb-card href="#">
+                    
+                <div class="p-5 w-35">        
+                    <img class="scale-50" src="../../assets/icons/plant.png" alt="">     
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {{ plant.name }}
+                    </h5>
+                    <fwb-badge> {{ plant.location }} </fwb-badge>
+                    <p class="font-normal text-gray-700 dark:text-gray-400">
+                    {{ plant.description }}
+                    </p>
+                </div>
+
+              </fwb-card>
+            </RouterLink>
+
+          </div>
         </div>
       </div>
     </div>
-  </template>
+  </div>
+</template>
   
+  <script setup>
+  import { 
+     FwbCard,
+     FwbBadge,
+     FwbSpinner,
+     FwbImg,
+    } from 'flowbite-vue'
+ </script>
+
   <script>
-  import axios from 'axios';
+  import axios from 'axios'
   
   export default {
     data() {
       return {
         loading: false,
         error: null,
-        plants: []
+        plants: [],
       };
     },
     mounted() {
@@ -41,7 +68,7 @@
             this.loading = false;
             this.error = error.message;
           });
-      }
+      },
     }
   }
   </script>
